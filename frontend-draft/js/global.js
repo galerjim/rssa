c94b313a3181c6a2e852123e09e6b3e8da0fc508
jacquelyn-rapose <jacquelynrapose@gmail.com>
2018-10-21 20:44:53 -0400

    survey page changes

--- frontend-draft/js/global.js
+++ frontend-draft/js/global.js
@@ -114,82 +114,33 @@ $(document).ready(function(){
    		$(location).attr('href', 'pg1.html');
 	});
 
-	nrOfQns = $('.question').length;
-	$('#surveypage1, #surveypage1button,#surveypage2, #surveypage2button, #surveypage3, #surveypage3button, #surveypage4, #surveypage4button').hide();
-	$(".question .rad_row td").click(function() {
-		$(this).find('input[type="radio"]').each(function() {
-			$(this).prop('checked', true);
-		});
-		$(this).parent().parent().css("border-left", "3px solid #00cc00");
+	//pg2 when choose buttin is clicked
+	$('span.choice').click(function(){
+		$(this).toggleClass("choice-button");
+ 		$(this).toggleClass("btn-primary");
 	});
 
 
+$('#surveypage2, #surveypage3,#surveypage4, #surveypage5, #surveypage6, #surveypage7, #surveypage8, #surveypage9, #surveypage10,#surveypage11,#surveypage12, #surveypage13, #surveypage14').hide();
 	
-	$('#surveypage1, #surveypage1button').show().fadeIn("slow");
+	let pageNo = 1;
+			$('#survey-next-button').click(function() {
 
-	
-
-		$('#surveypage1button').click(function() {
-		var isUnderProcess = $(this).attr('data-under-process');
-		if(typeof isUnderProcess !== typeof undefined && isUnderProcess == "true") {
-			console.warn("#surveypage1button :: Prevented multiple clicks.");
-			return;
-		}
-		$(this).attr("data-under-process", "true");
-		var hookpage = $('#surveypage1');
-		if (isSurveyComplete(hookpage)) {
-			postEvent('START_FINAL_SURVEY', {message: 'Started Final Survey'});
-			//go to page 2
-			secondanswers = saveSurveyResults(hookpage, secondanswers);
-			$('#surveypage1, #surveypage1button').fadeOut("slow", function() {
-				$('#surveypage1, #surveypage1button').hide();
-				$('#surveypage2, #surveypage2button').show().fadeIn("slow");
-			});
-		} else {
-			$(this).attr("data-under-process", "false");
-		}
-	});
-	
-	for(let pageNo = 2; pageNo <= 15; pageNo++) {
-		(function(pageNo) {
-			$('#surveypage' + pageNo + 'button').click(function() {
-				var isUnderProcess = $(this).attr('data-under-process');
-				if(typeof isUnderProcess !== typeof undefined && isUnderProcess == "true") {
-					console.warn('#surveypage' + pageNo + 'button :: Prevented multiple clicks.');
-					return;
-				}
-				$(this).attr("data-under-process", "true");
-				let hookpage = $("#surveypage" + pageNo);
-				if (isSurveyComplete(hookpage)) {
-					secondanswers = saveSurveyResults(hookpage, secondanswers);
-					$('#surveypage' + pageNo + ', #surveypage' + pageNo + 'button').fadeOut("slow", function() {
-						let nextPageNo = pageNo + 1;
-						$('#surveypage' + pageNo + ', #surveypage' + pageNo + 'button').hide();
-						$('#surveypage' + nextPageNo + ', #surveypage' + nextPageNo + 'button').show().fadeIn("slow");
+					if(pageNo != 14) {
+						$('#surveypage' + pageNo).hide().fadeOut("slow", function() {
+						pageNo = pageNo + 1;
+						$('#surveypage' + pageNo + '').show().fadeIn("slow");
 					});
-				} else {
-					$(this).attr("data-under-process", "false");
-				}
-			});
-		})(pageNo);
-	}
 
-	//last survey page
-	$('#surveypage16button').click(function() {
-		var isUnderProcess = $(this).attr('data-under-process');
-		if(typeof isUnderProcess !== typeof undefined && isUnderProcess == "true") {
-			console.warn("#surveypage16button :: Prevented multiple clicks.");
-			return;
-		}
-		$(this).attr("data-under-process", "true");
-		var hookpage = $('#surveypage16');
-		if (isSurveyComplete(hookpage)) {
-			secondanswers = saveSurveyResults(hookpage, secondanswers);
-			finish();
-		} else {
-			$(this).attr("data-under-process", "false");
-		}
-	});
+					} else {
+
+					}
+
+					
+				
+			});
+
+
 
 	
 
